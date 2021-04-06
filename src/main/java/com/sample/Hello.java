af1412b211a5f6697ab5630a5cdf3ede217adaa8
BGNene <80662705+BGNene@users.noreply.github.com>
2021-04-06 15:06:52 +0530

    MODIFIED1

--- src/main/java/com/sample/Hello.java
+++ src/main/java/com/sample/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 	System.out.println("Hello ECARE");
+	System.out.println("Hello ALL");
 
 	LocalDateTime  t=new LocalDateTime();
 	System.out.println("Currenrt Time is ::"+t);
