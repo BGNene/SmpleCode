7dcd0f29db803922a8d0879391c7131382881de7
BGNene <80662705+BGNene@users.noreply.github.com>
2021-04-06 14:37:48 +0530

    Nexus demo

---
+++ src/main/java/com/sample/Hello.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+import org.joda.time.LocalDateTime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println("Hello All");
+
+	LocalDateTime  t=new LocalDateTime();
+	System.out.println("Currenrt Time is ::"+t);
+	}
+
+}
