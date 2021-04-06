74bd939361a618fd65f76661e22843182ce39791
BGNene <80662705+BGNene@users.noreply.github.com>
2021-04-06 15:02:14 +0530

    modified

--- src/main/java/com/sample/Hello.java
+++ src/main/java/com/sample/Hello.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalDateTime;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello All");
+	System.out.println("Hello ECARE");
 
 	LocalDateTime  t=new LocalDateTime();
 	System.out.println("Currenrt Time is ::"+t);
